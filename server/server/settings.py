32e41643ae5122a2f683760fd8503fdafc208279
RexhinoDurro <rexhinodurro23@gmail.com>
2025-05-15 14:39:21 +0200

    \

--- server/server/settings.py
+++ server/server/settings.py
@@ -63,9 +63,9 @@ ROOT_URLCONF = 'server.urls'
 # CORS settings addition
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
+    "http://localhost:3001",
     "http://localhost:5173",
-    "http://127.0.0.1:3000",
+    "http://127.0.0.1:3001",
     "http://127.0.0.1:5173",
     "http://127.0.0.1:5174",
 ]
