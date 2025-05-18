8a8a49ca79c6fcf3a3cc0dff181e46ec67e6b229
RexhinoDurro <rexhinodurro23@gmail.com>
2025-05-18 23:37:48 +0200

    solidal

--- server/server/settings.py
+++ server/server/settings.py
@@ -68,10 +68,8 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3001",
     "http://localhost:5173",
-    "http://127.0.0.1:3001",
-    "http://127.0.0.1:5173",
-    "http://127.0.0.1:5174",
     "http://tolatiles.com"
+    "https://solidal.onrender.com"
 ]
 
 CORS_ALLOW_METHODS = [
