bbbea414dd193ff0f01032e034b8b6fdc61c1b5c
Solidalprojects <solidalprojects@gmail.com>
2025-05-18 21:59:44 +0200

    Update urls.py

--- server/server/urls.py
+++ server/server/urls.py
@@ -18,13 +18,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.http import HttpResponse  # Add this import
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
-    path('', home),
 ]
 
 # Serve media files in development
